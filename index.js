aa43ed6f7b87e1553c283f59d4e054ccc0f7247c
azifcodes <61082527+azifcodes@users.noreply.github.com>
2020-06-20 01:31:51 +0400

    Update index.js

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const count = document.getElementById("count");
 
 var cnt = 0;
 
-btn.onclick = function()
+btn.onclick = function ()
 {
-		let clr = Math.floor(Math.random()*16777215).toString(16);
-		
-		this.style.borderColor = "#" + clr;
-		this.style.color = "#" + clr;
-		this.style.boxShadow = "0 0 10vh #" + clr;
-		
-		cnt++;
-		count.textContent = cnt;
+    let clr = Math.floor(Math.random() * 16777215).toString(16);
+
+    this.style.borderColor = "#" + clr;
+    this.style.color = "#" + clr;
+    this.style.boxShadow = "0 0 10vh #" + clr;
+
+    cnt++;
+    count.textContent = cnt;
 }
